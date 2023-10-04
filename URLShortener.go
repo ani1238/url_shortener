d3be4805c98e4f4afce38ebf5b4628a243003bba
ani1238 <anisumi1238@gmail.com>
2023-10-04 14:49:55 +0530

    added redis as kv store

--- URLShortener.go
+++ URLShortener.go
@@ -4,6 +4,15 @@ import "fmt"
 import "sync"
 import "net/http"
 
+func init() {
+	// Initialize the Redis client.
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+}
+
 type URLShortener struct {
 	mutex    sync.Mutex
 	mappings map[string]string
@@ -26,24 +35,28 @@ func (us *URLShortener) ShortenURL(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	longURL := r.FormValue("url")
 
-	us.mutex.Lock()
-	defer us.mutex.Unlock()
-
 	shortURL := generateShortURL()
-	us.mappings[shortURL] = longURL
+
+	// Store the mapping in Redis.
+	err := redisClient.Set(context.Background(), shortURL, longURL, 24*time.Hour).Err()
+	if err != nil {
+		http.Error(w, "Failed to store URL in Redis", http.StatusInternalServerError)
+		return
+	}
 
 	fmt.Fprintf(w, "Shortened URL: %s%s\n", us.baseURL, shortURL)
 }
 
 func (us *URLShortener) Redirect(w http.ResponseWriter, r *http.Request) {
-	shortURL := r.URL.Path[1:] // Remove leading '/'
+	shortURL := r.URL.Path[1:]
 
-	us.mutex.Lock()
-	defer us.mutex.Unlock()
-
-	longURL, exists := us.mappings[shortURL]
-	if !exists {
+	// Retrieve the long URL from Redis.
+	longURL, err := redisClient.Get(context.Background(), shortURL).Result()
+	if err == redis.Nil {
 		http.NotFound(w, r)
+		return
+	} else if err != nil {
+		http.Error(w, "Failed to fetch URL from Redis", http.StatusInternalServerError)
 		return
 	}
 
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ani1238/url_shortener
+
+go 1.21.1
